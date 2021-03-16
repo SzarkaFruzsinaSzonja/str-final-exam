37ed6d0f9a2ba6f606f587d2b247af18e7e2e045
SzarkaFruzsinaSzonja <73585309+SzarkaFruzsinaSzonja@users.noreply.github.com>
2021-03-16 22:04:10 +0100

    first commit

---
+++ src/app/common/navigation/page/heroes/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Hero } from 'src/app/model/hero';
+import { HeroService } from 'src/app/service/hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = new Hero();
+  heroList$: BehaviorSubject<Hero[]> = this.hService.list$;;
+
+  constructor(private hService: HeroService) {
+    this.hService.getAll();
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  phrase: string = '';
+
+  searchEvent(event: Event): void {
+    this.phrase = (event.target as HTMLInputElement).value;
+  }
+
+  columnKey: string = '';
+
+  onColumnSelect(key: string): void {
+    this.columnKey = key;
+  }
+}
